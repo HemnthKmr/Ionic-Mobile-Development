8631221a65da6a1da1f55df11133e6539f3d145a
HemnthKmr <62270596+HemnthKmr@users.noreply.github.com>
2021-07-09 13:06:19 +0530

    Add files via upload

---
+++ url-app/src/data.js
@@ -0,0 +1,31 @@
+import googleLogo from "./assets/google-icon.svg";
+import facebookLogo from "./assets/facebook-icon.svg";
+import twitterLogo from "./assets/twitter-icon.svg";
+import instagramLogo from "./assets/instagram-icon.svg";
+
+export const data = [
+    {
+        id: 1,
+        name: "Google",
+        url: "http://www.google.com",
+        logo: googleLogo
+    },
+    {
+        id: 2,
+        name: "Facebook",
+        url: "http://www.facebook.com",
+        logo: facebookLogo
+    },
+    {
+        id: 3,
+        name: "Instagram",
+        url: "http://www.instagram.com",
+        logo: instagramLogo
+    },
+    {
+        id: 4,
+        name: "Twitter",
+        url: "http://www.twitter.com",
+        logo: twitterLogo
+    },
+]
